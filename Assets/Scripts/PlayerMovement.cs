b76f692ed954e7a01a0704c249734b7928251ba6
willowcheng <e.wingus@Gmail.com>
2015-09-02 19:53:11 -0400

    Add input update

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,6 +3,11 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	private Animator playerAnimator;
+	private float moveHorizontal;
+	private float moveVertical;
+	private Vector3 movement;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +15,9 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		moveHorizontal = Input.GetAxisRaw ("Horizontal");
+		moveVertical = Input.GetAxisRaw ("Vertical");
+
+		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 	}
 }
