d4fd7d67e58e45343b4ff0262da2050d3508c6a2
willowcheng <e.wingus@Gmail.com>
2015-09-02 20:17:31 -0400

    Add comment

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,21 +10,31 @@ public class PlayerMovement : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
+		// Gather the Animator component from the Player GameObject
 		playerAnimator = GetComponent<Animator> ();
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		// Gather input from the keyboard
 		moveHorizontal = Input.GetAxisRaw ("Horizontal");
 		moveVertical = Input.GetAxisRaw ("Vertical");
 
 		movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
 	}
 
 	void FixedUpdate () {
+
+		// If the player's movement vector does not equal zero...
 		if (movement != Vector3.zero) {
+			// ...then play the jump animation.
 			playerAnimator.SetFloat ("Speed", 3f);
 		} else {
+			// Otherwise, don't play jump animation.
 			playerAnimator.SetFloat ("Speed", 0f);
 		}
 	}
