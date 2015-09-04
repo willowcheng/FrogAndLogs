9a676eec187bbc47eb9bb0f0e778daf594e131d4
willowcheng <e.wingus@Gmail.com>
2015-09-04 11:36:48 -0400

    Add pickup particles

--- Assets/Scripts/FlyPickup.cs
+++ Assets/Scripts/FlyPickup.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class FlyPickup : MonoBehaviour {
 
+	[SerializeField]
+	private GameObject pickupPrefab;
+
 	void OnTriggerEnter(Collider other) {
 
 		// If the Collider other is tagged with "Player"...
 		if (other.CompareTag ("Player")) {
+
+			// ...add the pickup particles...
+			Instantiate (pickupPrefab, transform.position, Quaternion.identity);
+
 			Destroy(gameObject);
+
 		}
 	}
 }
