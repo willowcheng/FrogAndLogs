22d902ab7267b1096d37413674b3b1284e425612
willowcheng <e.wingus@Gmail.com>
2015-09-04 11:26:35 -0400

    Script the fly pickup

---
+++ Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyPickup : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+
+		// If the Collider other is tagged with "Player"...
+		if (other.CompareTag ("Player")) {
+			Destroy(gameObject);
+		}
+	}
+}
