260d434824566969cf303f7cc49722a1d0fcbe80
willowcheng <e.wingus@Gmail.com>
2015-09-26 00:36:46 -0400

    Create enemy navigation and AI

---
+++ Assets/Scripts/BirdMovement.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdMovement : MonoBehaviour {
+
+	[SerializeField]
+	private Transform target;
+	private NavMeshAgent birdAgent;
+	private Animator birdAnimator;
+
+	// Use this for initialization
+	void Start () {
+		birdAgent = GetComponent<NavMeshAgent> ();
+		birdAnimator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Set the bird's destination
+		birdAgent.SetDestination (target.position);
+
+		// Measure the magnitude of the NavMeshAgent's velocity
+		float speed = birdAgent.velocity.magnitude;
+
+		// Pass the velocity to the animator component
+		birdAnimator.SetFloat ("Speed", speed);
+
+	}
+}
